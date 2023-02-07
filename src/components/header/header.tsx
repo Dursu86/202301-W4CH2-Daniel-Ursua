05f69a189f2e5ec727b6d1f1ad367a2daf2515fa
Daniel <dursuaf@gmail.com>
2023-02-07 20:05:55 +0100

    Add main components

---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src="/public/logo.png" alt="Company logo" />
+      </div>
+      <h1 className="header__title">Academy questionnaire</h1>
+    </header>
+  );
+}
---
+++ src/components/main/main.tsx
@@ -0,0 +1,9 @@
+import { Personal } from "../../features/personal.data/personal";
+
+export function Main() {
+  return (
+    <main>
+      <Personal />
+    </main>
+  );
+}
